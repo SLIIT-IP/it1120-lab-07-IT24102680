2afabe6cf8c07288ead5f6c8ffa20d68ffc63694
IT24102680 <it24102680@my.sliit.lk>
2024-09-19 14:29:43 +0530

    IT24102680 Lab7 Q2C

---
+++ Q2/IT24102680Lab7Q2C.java
@@ -0,0 +1,20 @@
+public class IT24102680Lab7Q2C
+{
+   public static void main(String[] args)
+   {
+
+	int i, j, k=5;
+	
+	for (i=1; i<=5; i++)
+	{
+
+	   for(j=1; j<=k; j++)
+	   {
+	      System.out.print(k );
+	   }
+	   k-=1;
+	   System.out.print("\n");
+	}
+
+   }
+}
